63b4a4ff83920178ef62ecc1821cb726c978d2c2
theuaniyo <theuaniyo@gmail.es>
2019-01-25 23:35:10 +0100

    salon prototipo final, luz y movimiento

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int moveSpeed;
+    private int jumpPower;
+    private String groundTag = "Ground";
+    private bool onGround;
+    private bool inWater;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveSpeed = 3;
+        jumpPower = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && (onGround || inWater))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals(groundTag))
+        {
+            onGround = true;
+        }
+    }
+}
