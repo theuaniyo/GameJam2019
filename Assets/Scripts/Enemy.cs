081ebd1717d054a1084043a41ad0c3fe1eff3813
theuaniyo <theuaniyo@gmail.es>
2019-01-26 19:27:44 +0100

    Inicio nivel ciudad, materiales bosque y ciudad

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,30 +1,99 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class enemy : MonoBehaviour
 {
-    private Transform enemyPosition;
-    public int moveSpeed;
-    public Transform[] route;
-    private float lerpValue;
+
+    public List<Transform> positionMarks;
+
+    private Vector3 startPosition;
+
+    private Vector3 endPosition;
+
+    private Transform tf;
+
+    private int i = 0;
+
+    private int numbMarks;
+
+    public int movementTime;
+
+    private float lerpValue = 0;
+
+    private bool following = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyPosition = GetComponent<Transform>();
+
+        tf = GetComponent<Transform>();
+
+        startPosition = tf.position;
+
+        numbMarks = positionMarks.Count;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        foreach (Transform t in route)
+        
+        if (!following)
         {
-            while (enemyPosition.position != t.position)
-            {
-                lerpValue += Time.deltaTime / moveSpeed;
-                enemyPosition.position = Vector3.Lerp(enemyPosition.position, t.position, lerpValue);
-                Debug.Log();
-            }
+            
+            endPosition = positionMarks[i].position;
+
+            Debug.Log(positionMarks[i].position);
+
+            following = true;
         }
+        else
+        {
+            if(lerpValue < 1)
+            {
+                lerpValue += Time.deltaTime / movementTime;
+
+                tf.position = Vector3.Lerp(new Vector3(startPosition.x, tf.position.y, startPosition.z),
+                                                   new Vector3(endPosition.x, tf.position.y, endPosition.z),
+                                                   lerpValue);
+            }
+            else
+            {
+                if(i < numbMarks - 1) {
+
+                    i++;
+
+                    startPosition = endPosition;
+
+                    following = false;
+
+                    lerpValue = 0;
+
+                }
+                else
+                {
+                    i = 0;
+
+                    startPosition = endPosition;
+
+                    following = false;
+
+                    lerpValue = 0;
+                }
+
+                
+            }
+            
+
+
+        }
+
+       
+
+        
+
+
     }
 }
