993427c82315ce1ceb5c3295083c6ff2fef37738
theuaniyo <theuaniyo@gmail.es>
2019-01-26 13:45:04 +0100

    prototipo personaje, implementando movimiento carril

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody rb;
-    private int moveSpeed;
-    private int jumpPower;
+    public int moveSpeed;
+    public int jumpPower;
     private String groundTag = "Ground";
-    private bool onGround;
-    private bool inWater;
+    private bool jumping;
+    private int saltos;
+    private Transform tf;
+
+    private Vector3 startPosition;
+
+    private Vector3 endPosition;
+
+    private bool subiendoCarril = false;
+
+    private bool bajandoCarril = false;
+
+    private float lerpValue = 0;
+
+    public float movementTime = 5;
+
+
+    
+    
+
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        moveSpeed = 3;
-        jumpPower = 5;
-    }
+
+        tf = GetComponent<Transform>();
+
+        saltos = 0;
+
+}
 
     // Update is called once per frame
     void Update()
@@ -31,17 +52,104 @@ public class Player : MonoBehaviour
             rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
         }
 
-        if (Input.GetKey(KeyCode.Space) && (onGround || inWater))
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+
+            saltos++;
+
+            if(saltos == 2)
+            {
+                jumping = true;
+            }
+
+
+            
         }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+           if(tf.position.z < 1 && !subiendoCarril && !bajandoCarril)
+            {
+
+                startPosition = tf.position;
+
+                endPosition = new Vector3(tf.position.x, tf.position.y, tf.position.z + 3);
+  
+                subiendoCarril = true;
+
+           }
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (tf.position.z > -2 && !bajandoCarril && !subiendoCarril)
+            {
+
+                startPosition = tf.position;
+
+                endPosition = new Vector3(tf.position.x, tf.position.y, tf.position.z - 3);
+
+                bajandoCarril = true;
+
+            }
+
+
+        }
+
+        if (subiendoCarril)
+        {
+            if(tf.position.z < 1)
+            {
+                lerpValue += Time.deltaTime / movementTime;
+                tf.position = Vector3.Lerp(new Vector3(tf.position.x, tf.position.y, startPosition.z), 
+                                            new Vector3(tf.position.x, tf.position.y, endPosition.z),
+                                            lerpValue);
+
+            }
+            else
+            {
+                lerpValue = 0;
+                subiendoCarril = false;
+            }
+
+        }
+
+        if (bajandoCarril)
+        {
+            if (tf.position.z > -2)
+            {
+                lerpValue += Time.deltaTime / movementTime;
+                tf.position = Vector3.Lerp(new Vector3(tf.position.x, tf.position.y, startPosition.z),
+                                            new Vector3(tf.position.x, tf.position.y, endPosition.z),
+                                            lerpValue);
+
+            }
+            else
+            {
+                lerpValue = 0;
+                bajandoCarril = false;
+            }
+
+
+
+        }
+
+
+
+
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag.Equals(groundTag))
         {
-            onGround = true;
+            jumping = false;
+
+            saltos = 0;
+
         }
     }
 }
